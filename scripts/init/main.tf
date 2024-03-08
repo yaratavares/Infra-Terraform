7128c17728566861a5433ba1dd980279c273ee78
Yara Tavares <yaracriistavares@gmail.com>
2024-03-07 21:46:59 -0300

    init project

---
+++ scripts/init/main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_version = "0.14.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "sa-east-1"
+  profile = "tf01"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "bucket-to-index-catalog-123"
+
+  tags = {
+    Name        = "Bucket to Index Catalog"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
